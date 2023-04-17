ea3bf3718aea186e3a0ea6c024c02086000d5e49
jwozniak <jakubwozniak0611@gmail.com>
2023-04-17 17:31:33 +0200

    Fixed minor issues

--- Lab04/src/Main.java
+++ Lab04/src/Main.java
@@ -22,27 +22,23 @@ public class Main {
                 case 1 -> {
                     System.out.print("Podaj nową ocene >> ");
                     double newGrade = input.nextDouble();
-                    if(newGrade >=2 && newGrade <= 5) {
+                    if(newGrade >=2 && newGrade <= 5)
                         myGradeList.addGrade(newGrade);
-                    }
                     else {System.out.println("Podano złą ocene");}
                 }
                 case 2 -> {
-                    if(myGradeList.isEmpty()){
+                    if(myGradeList.isEmpty())
                         System.out.println("Srednia: " + myGradeList.averageGrade());
-                    }
                     else {System.out.println("Tablica jest pusta");}
                 }
                 case 3 -> {
-                    if(myGradeList.isEmpty()){
+                    if(myGradeList.isEmpty())
                         System.out.println("Maksymalna: " + myGradeList.maxGrade());
-                    }
                     else {System.out.println("Tablica jest pusta");}
                 }
                 case 4 -> {
-                    if(myGradeList.isEmpty()){
+                    if(myGradeList.isEmpty())
                         System.out.println("Maksymalna: " + myGradeList.minGrade());
-                    }
                     else {System.out.println("Tablica jest pusta");}
                 }
                 default -> System.out.println("Zła opcja");
